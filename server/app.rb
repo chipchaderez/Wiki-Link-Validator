8be6dad2c117954a1fe7f60ca37e8793841b9ea0
chipchaderez <chipchaderez@gmail.com>
2016-07-01 09:27:08 +0000

    Raise errors

--- server/app.rb
+++ server/app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'sinatra/cross_origin'
 require 'base64'
+require 'open3'
+require 'fileutils'
 
 set :port, 8081
 set :bind, '0.0.0.0'
@@ -24,12 +26,17 @@ logs = Hash.new
 get '/validate/:url' do
   url = Base64.decode64(params['url'])
   timestamp = params['timestamp']
+  wikiName = url.split('/').last;
+  wikiDir = '%s_%s' % [wikiName, timestamp]
   
   # Clone wiki repo
-  Process.spawn('mkdir wikis')
-  Process.wait
-  Process.spawn('cd wikis && git clone %s' % url)
-  Process.wait
+  FileUtils.mkdir_p('wikis')
+  Open3.popen3('cd wikis && git clone %s %s' % [url, wikiDir]) do |stdin, stdout, stderr, wait_thr|
+    puts "stdout:" + stdout.read
+    unless /exit 0/ =~ wait_thr.value.to_s
+      halt 500  , stderr.read
+    end
+  end
   
   # Kill previous process if exists
   pid = pids[url]
@@ -41,19 +48,23 @@ get '/validate/:url' do
   end
 
   # Execute validation process
-  begin
-    wikiName = url.split('/').last;
-    logFile = "%s_%s" % [wikiName, timestamp]
-    pid = fork do
-      exec('cd .. && ./wiki-links-validity.py -d server/wikis/%s/ -l %s' % [wikiName, logFile])
-    end
-    pids.store(url, pid.to_i + 3)
+ begin
+    logFile = "%s/logs/%s" % [Dir.pwd, wikiDir]
+    FileUtils.mkdir_p('logs')
+    pid = Process.spawn('cd .. && ./wiki-links-validity.py -d server/wikis/%s/ -l %s' % [wikiDir, logFile])
+    pids.store(url, pid.to_i + 1)
     logs.store(url, logFile)
-  rescue Exception
+  rescue Exception => e
+    puts e.backtrace
+    halt 500  , e.message
   end
 end
 
 get '/log/:url' do
   url = Base64.decode64(params['url']);
-  IO.read("../%s" % logs[url])
+  begin
+    IO.read(logs[url])
+  rescue Exception => e
+    puts e.message
+  end
 end
